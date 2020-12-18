ed7933b52f9b411ad973fdd17e7a9bee335c0f05
dotiful <artdevjs@gmail.com>
2020-12-18 05:30:27 +0200

    bump

--- src/routes/magnet.js
+++ src/routes/magnet.js
@@ -3,7 +3,15 @@ import { torrent2magnet } from 'torrent2magnet';
 
 const router = Router();
 
-router.get('/', (req, res) => { res.json({ route: req.originalUrl }) });
+// router.get('/', (req, res) => { 
+//   res.json({ route: req.originalUrl }) 
+// });
+
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express!</h1>');
+  res.end();
+});
 
 router.post('/', (req, res) => {
   torrent2magnet(req.body.url).then(uri => {
