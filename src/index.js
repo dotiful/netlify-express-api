95318d851840647911e83b4a95758bb06e84a563
dotiful <artdevjs@gmail.com>
2020-12-18 10:44:18 +0200

    bump

--- src/index.js
+++ src/index.js
@@ -4,43 +4,32 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import serverless from "serverless-http";
 
-// import models from './models';
-import routes from './routes';
-
 const app = express();
 const router = express.Router();
-const path = require('path');
+
+import routes from './routes';
 
 // Application-Level Middleware
-
 app.use(cors());
-
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express!</h1>');
-  res.end();
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.use('/.netlify/functions/api', router);  // path must route to lambda
-
-// app.use((req, res, next) => {
-//   req.context = {
-//     models,
-//     me: models.users[1],
-//   };
-//   next();
+// router.get('/', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.write('<h1>Hello from Express!</h1>');
+//   res.end();
 // });
 
 // Routes
+app.use('/.netlify/functions/index', router);  // path must route to lambda
 app.use('/magnet', routes.magnet);
+app.use('/', router);
 
 // Start
 app.listen(process.env.PORT, () =>
   console.log(`Local app listening on port ${process.env.PORT}!`),
 );
 
+module.exports = app;
 module.exports.handler = serverless(app);
